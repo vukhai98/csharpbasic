7cf678f672f997fd7081cfc811b3ccbcd9eb3d77
ADMIN\Admin <vukhai27091998@gmail.com>
2021-10-24 23:25:07 +0700

    bai 1 kteam

---
+++ test/csharpbasic/Ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kteam 2343253");
+            Console.ReadKey();
+        }
+    }
+}
