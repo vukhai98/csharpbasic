65cd6011c7feff055be684bdc639a880757a17d6
ADMIN\Admin <vukhai27091998@gmail.com>
2021-10-27 12:42:10 +0700

    If Else

---
+++ test/csharpbasic/ExampleIfElse/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExampleIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strA, strB;
+            int A, B;
+            double Nghiem;
+           
+            Console.WriteLine("Chuong trinh giai phuong trinh Ax + B = 0");
+            Console.WriteLine("Moi ban nhap so A :");
+            strA = Console.ReadLine();
+            Console.WriteLine("Moi ban nhao so b :");
+            strB = Console.ReadLine();
+            if (int.TryParse(strA, out A) == false || int.TryParse(strB, out B) == false)
+            {
+                Console.WriteLine("Du lieu nhap sai !");
+
+            }
+            else
+            {
+                Console.WriteLine("\n Phuong trinh cua ban vua nhap la :{0}x + {1} = 0", A, B);
+                if (A==0)
+                {
+                    Console.WriteLine("\n Phuong trinh vo nghiem !");
+
+                }
+                else if (B==0)
+                {
+                    Console.WriteLine("\n Phuong trinh co nghiem x = 0");
+
+
+                }
+                else
+                {
+                    Nghiem = (double)-B / A;
+                    Console.WriteLine("\n Phuong trinh co nghiem x = {0} ", Nghiem);
+                }
+
+                       
+            }
+        }
+
+
+       
+    }
+}
---
+++ test/csharpbasic/IfElse/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region lack type condition ( Điều kiện loại thiếu)
+            //string k = "Kteam";
+            //if(k == "Kteam")
+            //Console.WriteLine("Free Education");
+            //Console.ReadKey();
+            #endregion
+
+            #region sufficient condition ( Điều kiện loại đủ)
+            string k = "HowKTeam";
+            if (k == "HowKteam")
+            {
+                Console.WriteLine("Free Education");
+            }
+            else if( k == "Kteam")
+            {
+                Console.WriteLine("HowKteam.com");
+                Console.WriteLine("Free Education");
+            }
+            else
+            {
+                if (k == "10-09")
+                {
+                    Console.WriteLine("Conntecting HowKteam....");
+                }
+                else
+                {
+                    Console.WriteLine(" Public website HowKteam.com");
+                }
+            }
+
+
+
+            #endregion
+            Console.ReadKey();
+
+        }
+    }
+}
