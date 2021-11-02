fb208d1203a511d8f979705a2242b9574f8ee8f7
ADMIN\Admin <vukhai27091998@gmail.com>
2021-11-02 15:52:01 +0700

    three dimensional array jagged

---
+++ test/csharpbasic/ArrayJagged/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ArrayJagged
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Khai báo 1 mảng jagged tên jarray và có 2 phần tử tương ứng với 2 mảng con 
+             * mảng con thứ nhất là mảng có 3 phần tử.
+             * mảng con thứ hai là mảng có 7 phần tử.
+             */
+            int[][] jarray =
+            {
+                new int[] {1 ,2, 3},
+                new int[] {3, 4, 5, 6, 7, 8, 9}
+
+            };
+            for (int i = 0; i < jarray.Length; i++)
+            {
+                for (int j = 0; j < jarray[i].Length; j++)
+                {
+                    Console.Write("\t" + jarray[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            
+
+        }
+    }
+}
---
+++ test/csharpbasic/ThreeDimensionalArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ThreeDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Khởi tạo và cấp giá trị threeDimensionalArray
+            //int[,,] threeDimensionalArray = new int[,,]
+            //{
+            //   {
+            //       {1, 2, 3 },
+            //       {4, 5, 6 }
+            //   },
+            //   {
+            //       {7, 8, 9 },
+            //       {10, 11, 12 }
+            //   }
+            //};
+            //Console.WriteLine(threeDimensionalArray[0, 0, 0]);
+            //Console.ReadKey();
+            #endregion
+
+            #region Cách duyệt mảng 3 chiều
+            int[,,] intArray = new int[3, 9, 10];
+            for (int i = 0; i <intArray.GetLength(0); i++)
+            {
+                for (int j = 0; j <intArray.GetLength(1) ; j++)
+                {
+                    for (int k = 0; k <intArray.GetLength(2); k++)
+                    {
+                        Console.Write("\t" + intArray[i, j, k]);
+                        /*
+                         * Với cách duyệt này thì intArray [i,j,k] là phần tử hiện tại mình đang xét 
+                         * Code xử lý sẽ viết ở đây 
+                         */
+                    }
+                    Console.WriteLine();
+                }
+            }
+            #endregion
+        }
+    }
+}
