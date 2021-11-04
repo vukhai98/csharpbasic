3f77f4ee492fb35fe3861674edbf2c998f79b83a
ADMIN\Admin <vukhai27091998@gmail.com>
2021-11-04 13:39:10 +0700

    class String class StingBuilder

---
+++ test/csharpbasic/ClassString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName;
+            string result = "";
+            Console.Write(" Moi ban nhap ten minh :");
+            fullName = Console.ReadLine();
+            // cắt khoẳng trắng dư ở đầu và cuối chuỗi 
+            fullName = fullName.Trim();
+            // thực hiện tìm và thay thế 2 khoảng trắng or nhiều hơn thành 1 khoảng trắng 
+            while (fullName.IndexOf("  ")  != -1)
+            {
+                fullName = fullName.Replace(" ", " ");
+            }
+            /*
+             * Cắt chuỗi họ tên ra thành các mảng từ.
+             * Sau đó  duyệt mảng để chuẩn hóa từng từ một ,
+             * Khi duyệt mỗi từ ta thực hiện cắt ra chữ cái đầu tiên và lưu vào trong biếng FirstChar
+             * Các chữ còn lại lưu vào biến OtherChar 
+             * Thực hiện viết hoa chữ cái đầu và viết thường các chữ cái còn lại.
+             * cuối cùng là lưu trữ và chuẩn hóa vào biến result
+             */
+
+            string[] subName = fullName.Split(' '); 
+
+            for (int i = 0; i < subName.Length; i++)
+            {
+                string firstChar = subName[i].Substring(0, 1);
+                string otherChar = subName[i].Substring(1);
+                subName[i] = firstChar.ToUpper() + otherChar.ToLower();
+                result += subName[i] + " ";
+            }
+            Console.WriteLine("Ho ten cua ban la :{0}", result);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ test/csharpbasic/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Khai báo 1 biến kiểu chuỗi tên là FullName
+             * Khai báo 1 biến Result chứa kết quả chuẩn hoá chuỗi.
+             * Giá trị biến FullName được nhập từ bàn phím.
+             */
+            string FullName;
+            string Result = "";
+
+            Console.Write(" Moi ban nhap ho va ten: ");
+            FullName = Console.ReadLine();
+
+            /* Cắt các khoảng trắng dư ở đầu và cuối chuỗi */
+            FullName = FullName.Trim();
+
+            /* 
+             * Trong khi còn tìm thấy 2 khoảng trắng
+             * thì thực hiện thay thế 2 khoảng trắng bằng 1 khoảng trắng
+             */
+            while (FullName.IndexOf("  ") != -1)
+            {
+                FullName = FullName.Replace("  ", " ");
+            }
+            /*
+             * Cắt chuỗi họ tên ra thành mảng các từ.
+             * Sau đó duyệt mảng để chuẩn hoá từng từ một.
+             * Khi duyệt mỗi từ ta thực hiện cắt ra chữ cái đầu trên và lưu trong biến FirstChar
+             * Cắt các chữ cái còn lại và lưu trong biến OtherChar.
+             * Thực hiện viết hoa chữ cái đầu và viết thường các chữ cái còn lại.
+             * Cuối cùng là lưu chữ vừa chuẩn hoá vào biến Result.
+             */
+            string[] SubName = FullName.Split(' ');
+
+            for (int i = 0; i < SubName.Length; i++)
+            {
+                string FirstChar = SubName[i].Substring(0, 1);
+                string OtherChar = SubName[i].Substring(1);
+                SubName[i] = FirstChar.ToUpper() + OtherChar.ToLower();
+                Result += SubName[i] + " ";
+            }
+
+            Console.WriteLine(" Ho ten cua ban la: " + Result);
+        }
+    }
+}
